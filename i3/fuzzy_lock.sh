d4be1b71cb14cf9d312aaeae804700cb21f472c2
Yi Zhang <starain.zhang@gmail.com>
2017-12-24 14:48:12 -0800

    Performance improvement for fuzzy lock.

--- i3/fuzzy_lock.sh
+++ i3/fuzzy_lock.sh
@@ -5,11 +5,8 @@ lock_img=/tmp/screen_locked.png
 # Take a screenshot
 scrot $lock_img
 
-# Pixellate it 50x
-mogrify -scale 1.25% -scale 8000% $lock_img
-
-# Blur it
-convert $lock_img -blur 0x5 $lock_img
+# Pixellate it 50x then blur it
+cmd="convert $lock_img -scale 1.25% -scale 8000% -blur 0x5"
 
 # Embed xkcd
 url=$(curl -L https://c.xkcd.com/random/comic/ | grep "(for hotlinking/embedding)" | cut -d " " -f 5)
@@ -17,14 +14,19 @@ if [ "$url" != "" ]
 then
   xkcd_img=/tmp/xkcd.png
   curl -o $xkcd_img $url
-  s_h=$(identify -format "%h" $lock_img)
-  s_w=$(identify -format "%w" $lock_img)
-  e_h=$(identify -format "%h" $xkcd_img)
-  e_w=$(identify -format "%w" $xkcd_img)
+  s=$(identify -format "%h %w" $lock_img)
+  s_h=$(echo "$s" | cut -f 1 -d \ )
+  s_w=$(echo "$s" | cut -f 2 -d \ )
+  s=$(identify -format "%h %w" $xkcd_img)
+  e_h=$(echo "$s" | cut -f 1 -d \ )
+  e_w=$(echo "$s" | cut -f 2 -d \ )
   px=$((s_w / 2 - e_w / 2))
   py=$((s_h / 2 - e_h / 2))
-  convert $lock_img \( $xkcd_img -repage +$px+$py \) -layers flatten $lock_img
+  # Embed the image to the middle of the screen.
+  cmd="$cmd ( $xkcd_img -repage +$px+$py ) -layers flatten"
 fi
+cmd="$cmd $lock_img"
+$cmd
 
 # Lock screen displaying this image.
 i3lock -i $lock_img
